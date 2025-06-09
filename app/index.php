4d31355e9eedb04deb1a8c282cd16bb9824c60ee
Alexey Semenov <midhey@yandex.ru>
2025-06-09 18:50:50 +0300

    feat(pages): add homepage

---
+++ app/index.php
@@ -0,0 +1,9 @@
+<?php
+
+require_once __DIR__ . '/views/components/header.php';
+
+require_once BASE_PATH . '/controllers/locations.php';
+require_once BASE_PATH . '/controllers/services.php';
+require_once BASE_PATH . '/controllers/workers.php';
+
+require_once BASE_PATH . '/views/components/footer.php';
