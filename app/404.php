5171dcbc81e9196129ffe8e6b737e6caf4bbbb39
Alexey Semenov <midhey@yandex.ru>
2025-06-09 18:39:24 +0300

    feat(pages): add 404 page

---
+++ app/404.php
@@ -0,0 +1,5 @@
+<?php
+
+require_once __DIR__ . '/views/components/header.php';
+require_once BASE_PATH . '/views/components/not-found.php';
+require_once BASE_PATH . '/views/components/footer.php';
